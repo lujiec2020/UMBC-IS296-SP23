428129cc4ce791aceaad296c4b340252dd41d199
lujiec2020 <lujiec2020@gmail.com>
2023-02-27 12:36:17 -0500

    delete

--- hw/hw03/tests/q1_7.py
+++
@@ -1,4 +0,0 @@
-OK_FORMAT = True
-test = {   'name': 'q1_7',
-    'points': 1,
-    'suites': [{'cases': [{'code': '>>> highPTER == True or highPTER == False\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
--- hw/hw03/tests/q2_5.py
+++
@@ -1,8 +0,0 @@
-OK_FORMAT = True
-test = {   'name': 'q2_5',
-    'points': 1,
-    'suites': [   {   'cases': [{'code': '>>> type(visualization) == int\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> 1 <= visualization <= 3\nTrue', 'hidden': False, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
